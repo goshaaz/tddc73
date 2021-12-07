348dd0fc5b2af577010313cd555c29177d502f6c
Georgij Li <georgijli@mbpsomtrgeorgij.mynet>
2021-12-07 10:16:19 +0100

    project setup

---
+++ project/projectflutter/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      theme: ThemeData(primarySwatch: Colors.lightBlue),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+          AppBar(title: Text('Project', style: TextStyle(color: Colors.white))),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
